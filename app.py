67cda95ff32da7e466b0f737cca1f745e2dd818b
wonx <2736639+wonx@users.noreply.github.com>
2023-03-20 17:22:19 -0400

    Added line R7

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ estacions_r1=("L'Hospitalet de Llobregat", "Barcelona - Sants", "Barcelona - Pla
 estacions_r2=("Maçanet-Massanes", "Hostalric", "Riells i Viabrea-Breda", "Gualba", "Sant Celoni", "Palautordera", "Llinars del Vallès", "Cardedeu", "Les Franqueses del Vallès-Granollers Nord", "Granollers Centre", "Montmeló", "Mollet-Sant Fost", "La Llagosta", "Montcada i Reixac", "Barcelona - Sant Andreu", "Barcelona-El Clot-Aragó", "Barcelona - Estació de França", "Barcelona-Passeig de Gràcia ", "Barcelona-Sants", "Bellvitge", "El Prat de Llobregat", "Aeroport", "Viladecans", "Gavà", "Castelldefels", "Platja de Castelldefels", "Garraf", "Sitges", "Vilanova i la Geltrú", "Cubelles", "Cunit", "Segur de Calafell", "Calafell", "Sant Vicenç de Calders")
 estacions_r3=("L'Hospitalet de Llobregat", "Barcelona-Sants", "Barcelona-Plaça Catalunya", "Barcelona-Arc de Triomf", "Barcelona-La Sagrera-Meridiana", "Sant Andreu Arenal", "Torre del Baró-Vallbona", "Montcada Bifurcació", "Montcada Ripollet", "Santa Perpètua de Mogoda-La Florida", "Mollet-Santa Rosa", "Parets del Vallès", "Granollers-Canovelles", "Les Franqueses del Vallès", "La Garriga", "Figaró", "Sant Martí de Centelles", "Centelles", "Balenyà-Els Hostalets", "Balenyà-Tona-Seva", "Vic", "Manlleu", "Torelló", "Borgonyà", "Sant Quirze de Besora", "La Farga de Bebié", "Ripoll", "Campdevànol", "Ribes de Freser", "Planoles", "Toses", "La Molina", "Urtx-Alp", "Puigcerdà","Latour-de-Carol-Enveig")
 estacions_r4=("St. Vicenç de Calders", "El Vendrell", "L'Arboç", "Els Monjos", "Vilafranca del Penedès", "La Granada", "Lavern-Subirats", "St. Sadurní d'Anoia", "Gelida", "Martorell", "Castellbisbal", "El Papiol","Molins de Rei","St. Feliu de Llobregat","St. Joan Despí","Cornellà","L'Hospitalet de Llobregat","Barcelona Sants","Barcelona Plaça de Catalunya","Barcelona Arc de Triomf","Barcelona La Sagrera-Meridiana","Barcelona St.Andreu Arenal","Barcelona Torre del Baró","Montcada Bifurcació","Montcada i Reixac-Manresa","Montcada i Reixac-Sta. Maria","Cerdanyola del Vallès","Barberà del Vallès","Sabadell Sud","Sabadell Centre","Sabadell Nord","Terrassa Est" ,"Terrassa" ,"St. Miquel de Gonteres" ,"Viladecavalls" ,"Vacarisses-Torreblanca" ,"Vacarisses" ,"Castellbell i el Vilar-Monistrol de Montserrat" ,"St. Vicenç de Castellet" ,"Manresa")
+estacions_r7=('Barcelona St. Andreu Arenal','Barcelona Torre del Baró','Montcada Bifurcació','Montcada i Reixac-Manresa','Montcada i Reixac-Sta. Maria', 'Cerdanyola del Vallès','Cerdanyola Universitat')
 estacions_r8=('Martorell', 'Castellbisbal', 'Rubí', 'Sant Cugat del Vallès', 'Cerdanyola Universitat', 'Santa Perpètua de Mogoda - Riera de caldes', 'Mollet - Sant Fost', 'Montmeló', 'Granollers Centre')
 
 area_r1_anada_feiners = (605, 10, 2360, 950)
@@ -23,6 +24,8 @@ area_r3_anada_feiners = (555, 10, 1500, 960)
 area_r3_tornada_feiners = (557, 10, 1450, 960)
 area_r4_anada_feiners = (576, 10, 2320, 965)
 area_r4_tornada_feiners = (576, 10, 2320, 965)
+area_r7_anada = (620, 140, 1650, 800)
+area_r7_tornada = (620, 140, 1600, 800)
 area_r8_anada = (468, 10, 1300, 950)
 area_r8_tornada = (1625, 10, 2300, 950)
 
@@ -38,6 +41,18 @@ schedule = tabula.read_pdf(url, pages=2, stream=True, lattice=False, multiple_ta
 schedule[0].columns = estacions_r1[::-1]
 r1_tornada_feiners = helpers.cleanup_trainschedule(schedule[0])
 
+# R7 (Bcn St Andreu-Cerdanyola Universitat)
+url = "https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R7.pdf"
+schedule = tabula.read_pdf(url, pages=1, stream=True, lattice=False, multiple_tables=True, area=area_r7_anada)
+schedule[0].columns = estacions_r7
+r7_anada = helpers.cleanup_trainschedule(schedule[0])
+
+# R7 (Cerdanyola Universitat - Bcn St Andreu)
+url = "https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R7.pdf"
+schedule = tabula.read_pdf(url, pages=2, stream=True, lattice=False, multiple_tables=True, area=area_r7_tornada)
+schedule[0].columns = estacions_r7[::-1]
+r7_tornada = helpers.cleanup_trainschedule(schedule[0])
+
 # R8 (Martorell-Granollers)
 url = "https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R8.pdf"
 schedule = tabula.read_pdf(url, pages=1, stream=True, lattice=False, multiple_tables=True, area=area_r8_anada)
@@ -49,7 +64,6 @@ url = "https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R8.pdf"
 schedule = tabula.read_pdf(url, pages=1, stream=True, lattice=False, multiple_tables=True, area=area_r8_tornada)
 schedule[0].columns = estacions_r8[::-1]
 r8_tornada = helpers.cleanup_trainschedule(schedule[0])
-print(r8_tornada)
 
 # R2 (Maçanet - St. Vicenç de Calders)
 url = "https://rodalies.gencat.cat/web/.content/02_Horaris/horaris/R2.pdf"
@@ -138,6 +152,11 @@ def generate_data():
                     "trainLine": "Rodalies R4",
                     "positions1": helpers.find_alltrains(r4_anada_feiners, bcn_time),
                     "positions2": helpers.find_alltrains(r4_tornada_feiners, bcn_time, inverse=True)
+                },
+                                {
+                    "trainLine": "Rodalies R7",
+                    "positions1": helpers.find_alltrains(r7_anada, bcn_time),
+                    "positions2": helpers.find_alltrains(r7_tornada, bcn_time, inverse=True),
                 },
                 {
                     "trainLine": "Rodalies R8",
