3d713a08b4f988d83d05693fe8164109b4520c4b
wonx <2736639+wonx@users.noreply.github.com>
2023-03-24 10:10:08 -0400

    Added support for multiple lines i map. R8 and R3 now being displayed

--- static/js/scriptmap.js
+++ static/js/scriptmap.js
@@ -4,20 +4,37 @@ var svgDoc;
 mySVG.addEventListener("load", function() {
     svgDoc = mySVG.contentDocument;
     
-    var stationPositions = [0, 0.0935, 0.1335, 0.16, 0.2345, 0.256, 0.278, 0.2985, 0.3445, 0.3593, 0.3845, 0.4000, 0.4475, 0.4782, 0.539, 0.5621, 0.585, 0.608, 0.630, 0.653, 0.6985, 0.7165, 0.731, 0.7543, 0.7727, 0.791, 0.809, 0.8355, 0.872, 0.8903, 0.9085, 0.9268, 0.9451, 0.9815, 1];
-    addStations(svgDoc, stationPositions);
+    var stationPositions = [0, 0.0295, 0.055, 0.208, 0.4237, 0.499, 0.6837, 0.915, 1];
+    var pathId = "path9845"
+    //addStations(svgDoc, pathId, stationPositions);
+
+    // Paths - lines correspondence in svg file:
+    // R1: path9386
+    // R2 nord: path9390
+    // R2 centre: path9552
+    // R2 sud: path9388
+    // R3: path9823_joined
+    // R4: path9534
+    // R5: path9518
+    // R50: path9518-1
+    // R6: path9514
+    // R60: path9512 + path9514-9
+    // R7: path9845-4
+    // R8: path9845
 
     // Example usage: add a marker at station 2.5 (between station 2 and 3)
     //setTrainMarkers(svgDoc, 3.15, stationPositions, "green");
 });
 
 // Places two sets of positions on the train map. The positions of the stations need to be passed too, as well as the colors of the markers.
-function setTrainMarkers(svgDoc, trainPositions1, trainPositions2, stationPositions, color1, color2) {
-  var path = svgDoc.getElementById("path9823_joined");
+function setTrainMarkers(svgDoc, pathId, trainPositions1, trainPositions2, stationPositions, color1, color2) {
+  var path = svgDoc.getElementById(pathId);
   var pathLength = path.getTotalLength();
 
   // Remove existing markers with class name "train-marker"
-  var markers = path.parentNode.querySelectorAll('.train-marker');
+  var markers = path.parentNode.querySelectorAll(".train-marker_"+pathId);
+  console.log("path", path)
+  console.log("Markers:", markers)
   for (var i = markers.length - 1; i >= 0; i--) {
       markers[i].parentNode.removeChild(markers[i]);
   }
@@ -37,7 +54,7 @@ function setTrainMarkers(svgDoc, trainPositions1, trainPositions2, stationPositi
       marker.setAttribute("fill", color);
       marker.setAttribute("stroke", "black");
       marker.setAttribute("stroke-width", "1");
-      marker.classList.add("train-marker");
+      marker.classList.add("train-marker_"+pathId);
 
       path.parentNode.appendChild(marker);
     }
@@ -51,8 +68,8 @@ function setTrainMarkers(svgDoc, trainPositions1, trainPositions2, stationPositi
 }
 
 // Draws the position of the stations on the map. Not strictly necessary, but useful to determine their position
-function addStations(svgDoc, stationPositions) {
-  var path = svgDoc.getElementById("path9823_joined");
+function addStations(svgDoc, pathId, stationPositions) {
+  var path = svgDoc.getElementById(pathId);
   var pathLength = path.getTotalLength();
 
   for (var i = 0; i < stationPositions.length; i++) {
@@ -62,7 +79,7 @@ function addStations(svgDoc, stationPositions) {
     station.setAttribute("cx", stationPos.x);
     station.setAttribute("cy", stationPos.y);
     station.setAttribute("r", 3);
-    station.setAttribute("fill", "purple");
+    station.setAttribute("fill", "yellow");
 
     path.parentNode.appendChild(station);
   }
@@ -75,11 +92,14 @@ eventSource.onmessage = function(event) {
   console.log(data);
   console.log(svgDoc)
   var r3stationPositions = [0, 0.0935, 0.1335, 0.16, 0.2345, 0.256, 0.278, 0.2985, 0.3445, 0.3593, 0.3845, 0.4000, 0.4475, 0.4782, 0.539, 0.5621, 0.585, 0.608, 0.630, 0.653, 0.6985, 0.7165, 0.731, 0.7543, 0.7727, 0.791, 0.809, 0.8355, 0.872, 0.8903, 0.9085, 0.9268, 0.9451, 0.9815, 1];
+  var r8stationPositions = [1, 0.915, 0.6837, 0.499, 0.4237, 0.208, 0.055, 0.0295, 0];
   //data.forEach(line => {
   //  setTrainMarkers(svgDoc, line.positions1[0], r3stationPositions, "green");
   //});
   console.log(data[2]) //data[2] es la linia R3
-  setTrainMarkers(svgDoc, data[2].positions1, data[2].positions2, r3stationPositions, "yellow", "orange");
-  //setTrainMarkers(svgDoc, data[2].positions2[0], r3stationPositions, "orange");
+  setTrainMarkers(svgDoc, "path9823_joined", data[2].positions1, data[2].positions2, r3stationPositions, "yellow", "orange");
+  setTrainMarkers(svgDoc, "path9845", data[5].positions1, data[5].positions2, r8stationPositions, "yellow", "orange");
+
   
 };
+
