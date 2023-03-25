9c0c8d1ffdb7c8c204ae3eae274eaa995f052079
wonx <2736639+wonx@users.noreply.github.com>
2023-03-25 00:27:49 -0400

    Added R2 south to live map

--- static/js/scriptmap.js
+++ static/js/scriptmap.js
@@ -4,17 +4,12 @@ var svgDoc;
 mySVG.addEventListener("load", function() {
     svgDoc = mySVG.contentDocument;
     
-    var stationPositions = [1.000, 0.9543, 0.9300, 0.9025, 
-      0.869, 0.8457, 0.8232, 0.803, 
-      0.7875, 0.7573, 0.7195, 0.6988, 
-      0.6626, 0.6454, 0.6284, 0.6123,
-      0.5905, 0.5273, 0.5001, 0.482,
-      0.4291, 0.4142, 0.3993, 0.3895,
-      0.3377, 0.321, 0.3039, 0.2733,
-      0.2564, 0.2375, 0.2212, 0.1531,
-      0.1375, 0.1085, 0.0935, 0.0785,
-      0.0638, 0.0487, 0.034 , 0.0000]
-    var pathId = "path9534"
+    var stationPositions = [0.0000, 0.172, 0.284, 0.403, 
+      0.456, 0.5510, 0.5870, 0.6280, 
+      0.6600, 0.6960, 0.7485, 0.7832, 
+      0.8250, 0.8670, 0.9070, 0.9490, 
+      1.0000]
+    var pathId = "path9388"
     //addStations(svgDoc, pathId, stationPositions);
 
     // Paths - lines correspondence in svg file:
@@ -98,6 +93,11 @@ eventSource.onmessage = function(event) {
   const data = JSON.parse(event.data);
   console.log(data);
   console.log(svgDoc)
+  var r2sudstationPositions = [0.0000, 0.172, 0.284, 0.403, 
+    0.456, 0.5510, 0.5870, 0.6280, 
+    0.6600, 0.6960, 0.7485, 0.7832, 
+    0.8250, 0.8670, 0.9070, 0.9490, 
+    1.0000]
   var r3stationPositions = [0, 0.0935, 0.1335, 0.16, 0.2345, 
     0.256, 0.278, 0.2985, 0.3445, 
     0.3593, 0.3845, 0.4000, 0.4475, 
@@ -121,13 +121,13 @@ eventSource.onmessage = function(event) {
   //data.forEach(line => {
   //  setTrainMarkers(svgDoc, line.positions1[0], r3stationPositions, "green");
   //});
-  console.log(data[3]) //data[2] es la linia R3
+  console.log(data[4]) //data[2] es la linia R3
   color1 = "yellow"
   color2 = "orange"
+  setTrainMarkers(svgDoc, "path9388", data[4].positions1, data[4].positions2, r2sudstationPositions, color1, color2); // R2 south
   setTrainMarkers(svgDoc, "path9823_joined", data[5].positions1, data[5].positions2, r3stationPositions, color1, color2); //R3
   setTrainMarkers(svgDoc, "path9534", data[6].positions1, data[6].positions2, r4stationPositions, color1, color2); //R4
   setTrainMarkers(svgDoc, "path9845-4", data[9].positions1, data[9].positions2, r7stationPositions, color1, color2); //R7
   setTrainMarkers(svgDoc, "path9845", data[10].positions1, data[10].positions2, r8stationPositions, color1, color2); //R8
-
 };
 
