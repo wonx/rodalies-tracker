2b0203e1106f22ea3430538a9561ffce386492eb
wonx <2736639+wonx@users.noreply.github.com>
2023-04-05 16:29:19 -0400

    Moved functions from app.py to tabuladata.py

--- tabuladata.py
+++ tabuladata.py
@@ -231,3 +231,121 @@ def get_tabula_schedule(route, tornada=False):
 
 ## Usage: 
 #display(get_tabula_schedule("R5", tornada=True))
+
+
+
+
+# Line R2 is divided between R2 North, R2 and R2 South. However, this is not consistent and some trains overlap these lines.
+# Separates the R2 line into north, central and south portions
+def get_r2_nordcentresud(df):
+    # North
+    df_nord = df.copy()
+    df_nord = df_nord.drop(df_nord[df_nord['Castelldefels'].notna()].index) # Si surt Castelldefels, tampoc es Centre
+    df_nord = df_nord.drop(df_nord[df_nord['Barcelona - Estació de França'].notna()].index) # Si surt l'estació de França, tampoc es Nord
+    df_nord = df_nord.dropna(axis=0, how='all') # Esborrem les files buides
+    df_nord = df_nord.dropna(axis=1, how='all') # Esborrem les columnes buides
+
+    # Center
+    df_centre = df.copy()
+    df_centre = df_centre[(df_centre['Aeroport'].isnull()) & (df_centre['Barcelona - Estació de França'].isnull())] # Si paren a Aeroport o Estació de França, no poden ser R2 centre.
+    df_centre = df_centre.drop(df_centre[df_centre['Vilanova i la Geltrú'].notna()].index) # Si surt Vilanova, tampoc es Centre
+    df_centre = df_centre.drop(df_centre[df_centre['Sant Celoni'].notna()].index) # Si surt Sant Celoni, tampoc es Centre
+    df_centre = df_centre.dropna(axis=0, how='all') # Delete empty rows (not really necessary here)
+    df_centre = df_centre.dropna(axis=1, how='all') # Esborrem les columnes buides
+
+    # South
+    df_sud = df.copy()
+    df_sud = df_sud.dropna(subset=['Barcelona - Estació de França'], how='all') # Ens quedem només aquells que comencen a Estació de França
+    df_sud = df_sud.drop(df_sud[df_sud['Aeroport'].notna()].index) # Si surt l'aeroport, tampoc es Nord
+    df_sud = df_sud.dropna(axis=1, how='all') # Esborrem les columnes buides
+
+    # Other
+    #  Get the rows that did not fit any of the three dataframes, and create another one (df_other) 
+    index_subsets = pd.concat([df_nord, df_centre, df_sud]).index
+    index_original = ~df.index.isin(index_subsets)
+    df_other = df.loc[index_original]
+
+    #  Now split the others into two (until Passeig de Gràcia, and from Sants)
+    df_other_1 = df_other.loc[:, 'Maçanet-Massanes':'Barcelona-Passeig de Gràcia'] 
+    df_other_2 = df_other.loc[:, 'Barcelona-Sants':'Sant Vicenç de Calders']
+
+    #  If any lines contain Airport, it's North. If any lines contain Estació de França, it's south
+    #  then concat the remainder to their corresponding dataframe (north or south)
+    df_nord = pd.concat([df_nord, df_other_1[df_other_1['Barcelona - Estació de França'].isnull()]]).drop(columns='Barcelona - Estació de França')
+    df_sud = pd.concat([df_sud, df_other_2[df_other_2['Aeroport'].isnull()]]).drop(columns='Aeroport')
+
+    # Now deal with the train that starts at Estació de França but ends at Aeroport
+    df_sud = pd.concat([df_sud, df_other[df_other['Aeroport'].notnull() & df_other['Barcelona - Estació de França'].notnull()].loc[:, 'Barcelona - Estació de França':'Barcelona-Sants']])
+    df_nord = pd.concat([df_nord, df_other[df_other['Aeroport'].notnull() & df_other['Barcelona - Estació de França'].notnull()].loc[:, 'Barcelona-Sants':'Aeroport']])
+
+    df_nord.sort_index(inplace=True)
+    df_sud.sort_index(inplace=True)
+
+    return df_nord, df_centre, df_sud
+
+
+# R5 has 3 different services (R5, R50 and S4), depending on which stations the trains stop.
+def get_r5_services(df):
+    #R5 (Ends at Manresa Baixador (end of the line), doesn't stop at all stations)
+    df_r5  = df.copy()
+    df_r5 = df_r5.dropna(subset=['Aeri de Montserrat'], how='all') # Only R5 stops there.
+
+    #S4 (Ends at Olesa de Montserrat, stops at all stations)
+    df_s4  = df.copy()
+    # Stops at Martorell Enllaç but not at Aeri de Montserrat
+    df_s4 = df_s4.dropna(subset=['Martorell Enllaç'], how='all') 
+    df_s4 = df_s4.drop(df_s4[df_s4['Aeri de Montserrat'].notna()].index)
+
+    #R50 (Ends at Manresa Baixador, does not stop at all stations (even fewer than R5), like Aeri de Montserrat)
+    df_r50  = df.copy()
+    df_r50 = df_r50.dropna(subset=['Manresa-Baixador'], how='all')
+    df_r50 = df_r50.drop(df_r50[df_r50['Aeri de Montserrat'].notna()].index)
+
+    #  Alternative: Get the rows that did not fit any of the three dataframes, and create another one (df_other) 
+    #index_subsets = pd.concat([df_r5, df_s4]).index
+    #index_original = ~df.index.isin(index_subsets)
+    #df_r50 = df.loc[index_original]
+
+    return df_r5, df_s4, df_r50
+
+# R6 schedule includes R6 and R60
+def get_r6_services(df):
+    #R6 (Stops at all stations, like Martorell Enllaç)
+    df_r6  = df.copy()
+    df_r6 = df_r6.dropna(subset=['Martorell Enllaç'], how='all') # Only R5 stops there.
+
+    #R60 
+    df_r60  = df.copy()
+    df_r60 = df_r60.drop(df_r60[df_r60['Martorell Enllaç'].notna()].index) # Does not stop there
+
+    return df_r6, df_r60
+
+
+def get_s3_s8_s9_services(df, df_s4):
+    df_s3  = df.copy() # Ends at Can Ros. Stops at every station
+    # Keep station that stop at Colonia Güell, but not Quatre camins
+    df_s3 = df_s3.dropna(subset=['Colònia Güell'], how='all') # Only trains that stop there
+    df_s3 = df_s3.drop(df_s3[df_s3['Quatre Camins'].notna()].index) # But remove trains stopping there
+
+    df_s8 = df.copy() # Trains that end at Martorell Enllaç and Stop at every station, and are not part of S4
+    df_s8 = df_s8.dropna(subset=['Martorell Enllaç', 'Martorell Vila | Castellbisbal'], how='any')
+    df_s8.drop(df_s8[df_s8['Plaça Espanya'].isin(df_s4['Plaça Espanya'])].index, inplace=True) # Remove rows not in S4
+    
+    df_s9  = df.copy() # End at Quatre Camins. Stops at every station
+    df_s9 = df_s9.dropna(subset=['Quatre Camins', 'Colònia Güell'], how='any') # Only trains that stop there
+    df_s9 = df_s9.drop(df_s9[df_s9['Sant Andreu de la Barca'].notna()].index)
+
+    return df_s3, df_s8, df_s9
+
+
+# Gets lines S2 and S6 (Universitat Autonoma & Sabadell)
+def get_s2_s6_services(df):
+    #S6 (Ends at Universitat Autonoma, Stops at Sant Gervasi)
+    df_s6  = df.copy()
+    df_s6 = df_s6.dropna(subset=['Sant Gervasi'], how='all') # Only R5 stops there.
+    
+    #S2 (Goes until Sabadell Parc del Nord, the last stop)
+    df_s2  = df.copy()
+    df_s2 = df_s2.dropna(subset=['Sabadell Parc del Nord'], how='all') # Only R5 stops there.
+
+    return df_s2, df_s6
